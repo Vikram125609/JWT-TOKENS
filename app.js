83af83ba4ae4e18308299f360f2c2be08684d3b1
Vikram Singh <vs125609@gmail.com>
2022-05-09 17:50:10 +0530

    Token Verification Done

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const createToken = async () => {
-    const token = await jwt.sign({_id:"62701344091362348ad0c64e"},"mynameisvikramsinghdewdaamillionear")
+    const token = await jwt.sign({ _id: "62701344091362348ad0c64e" }, "mynameisvikramsinghdewdaamillionear",{
+        expiresIn:"2 minutes"
+    })
     console.log(token);
+    const userVerify = await jwt.verify(token, "mynameisvikramsinghdewdaamillionear")
+    console.log(userVerify);
 };
 createToken();
